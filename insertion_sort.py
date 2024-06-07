535a4d31581dbbc954cb5c48ae634724d7a20766
yashkevadiya2 <yashkevadiya2@gmail.com>
2024-06-07 21:07:59 +0200

    create insertion sort in python

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+def insertation_sort(arr):
+
+    n  = len(arr)
+
+    for i in range(1,n):
+        key = arr[i]
+        j = i - 1
+        while j>=0 and key<arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+
+arr = [10,2,10,56,8,96]
+
+insertation_sort(arr)
+
+print(arr)
